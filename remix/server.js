464efbb615cff066200c5e637bb419acb80d92f5
Joe <contact@joemethven.com>
2022-04-19 13:25:40 +0100

    Updated gitignore, updating server.js

--- remix/server.js
+++ remix/server.js
@@ -1,25 +1,25 @@
-import { createPagesFunctionHandler } from "@remix-run/cloudflare-pages";
-import { createCloudflareKVSessionStorage } from "@remix-run/cloudflare-pages"
-import * as build from "@remix-run/dev/server-build";
+import { createPagesFunctionHandler } from '@remix-run/cloudflare-pages';
+import { createCloudflareKVSessionStorage } from '@remix-run/cloudflare';
+import * as build from '@remix-run/dev/server-build';
 
 const handleRequest = createPagesFunctionHandler({
-  build,
-  mode: process.env.NODE_ENV,
-  getLoadContext: (context) => {
-    const sessionStorage = createCloudflareKVSessionStorage({
-      cookie: {
-        name: "__session",
-        secrets: ["v#nugZ.a/gB=OV|rO5Q7"],
-        secure: true,
-        sameSite: "strict",
-      },
-      kv: context.env.sessionStorage,
-    })
+	build,
+	mode: process.env.NODE_ENV,
+	getLoadContext: (context) => {
+		const sessionStorage = createCloudflareKVSessionStorage({
+			cookie: {
+				name: '__session',
+				secrets: ['v#nugZ.a/gB=OV|rO5Q7'],
+				secure: true,
+				sameSite: 'strict',
+			},
+			kv: context.env.sessionStorage,
+		});
 
-    return { sessionStorage }
-  },
+		return { sessionStorage };
+	},
 });
 
 export function onRequest(context) {
-  return handleRequest(context);
+	return handleRequest(context);
 }
