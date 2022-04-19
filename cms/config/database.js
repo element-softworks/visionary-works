cde261abe86c0d00f34fcaf48b3ef45d1f56d853
Luke Brown <me@luke.sx>
2022-04-19 17:17:43 +0100

    CMS DB changes

--- cms/config/database.js
+++ cms/config/database.js
@@ -1,11 +1,37 @@
-const path = require('path');
+const path = require("path");
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
+module.exports = ({ env }) => {
+  console.log(env("DATABASE_URL"))
+  if (!!env("DATABASE_URL")) {
+    const parse = require("pg-connection-string").parse;
+    const config = parse(env("DATABASE_URL"));
+
+    console.log({ config });
+    return {
+      connection: {
+        client: "postgres",
+        connection: {
+          host: config.host,
+          port: config.port,
+          database: config.database,
+          user: config.user,
+          password: config.password,
+          ssl: {
+            rejectUnauthorized: false
+          }
+        },
+        debug: false
+      }
+    };
+  }
+
+  return {
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-    },
-    useNullAsDefault: true,
-  },
-});
+      client: "sqlite",
+      connection: {
+        filename: path.join(__dirname, "..", env("DATABASE_FILENAME", ".tmp/data.db"))
+      },
+      useNullAsDefault: true
+    }
+  };
+};
