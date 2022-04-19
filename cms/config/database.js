5c6d48cc331b7773f2d7b4fe5a2ded1f9809102d
Joe <contact@joemethven.com>
2022-04-19 17:29:11 +0100

    DB Config change

--- cms/config/database.js
+++ cms/config/database.js
@@ -2,36 +2,26 @@ const path = require("path");
 
 module.exports = ({ env }) => {
   console.log(env("DATABASE_URL"))
-  if (!!env("DATABASE_URL")) {
-    const parse = require("pg-connection-string").parse;
-    const config = parse(env("DATABASE_URL"));
 
-    console.log({ config });
-    return {
-      connection: {
-        client: "postgres",
-        connection: {
-          host: config.host,
-          port: config.port,
-          database: config.database,
-          user: config.user,
-          password: config.password,
-          ssl: {
-            rejectUnauthorized: false
-          }
-        },
-        debug: false
-      }
-    };
-  }
+  const parse = require("pg-connection-string").parse;
+  const config = parse(env("DATABASE_URL"));
+
+  console.log({ config });
 
   return {
     connection: {
-      client: "sqlite",
+      client: "postgres",
       connection: {
-        filename: path.join(__dirname, "..", env("DATABASE_FILENAME", ".tmp/data.db"))
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        user: config.user,
+        password: config.password,
+        ssl: {
+          rejectUnauthorized: false
+        }
       },
-      useNullAsDefault: true
+      debug: false
     }
-  };
+  }
 };
